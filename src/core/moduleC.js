5e0de5ad1e62506979d6c650553b543e1f568d65
qq2574437083-cyber <qq2574437083@gmail.com>
2026-03-31 22:06:23 +0800

    Create moduleC.js with EventHandler class Implementation

---
+++ src/core/moduleC.js
@@ -0,0 +1,31 @@
+class EventHandler {
+    constructor() {
+        this.events = {};
+    }
+
+    on(event, callback) {
+        if (!this.events[event]) {
+            this.events[event] = [];
+        }
+        this.events[event].push(callback);
+    }
+
+    emit(event, data) {
+        if (this.events[event]) {
+            this.events[event].forEach(callback => {
+                try {
+                    callback(data);
+                } catch (error) {
+                    console.error(`Error occurred in event handler for ${event}:`, error);
+                }
+            });
+        }
+    }
+
+    off(event, callback) {
+        if (!this.events[event]) return;
+        this.events[event] = this.events[event].filter(cb => cb !== callback);
+    }
+}
+
+module.exports = EventHandler;
